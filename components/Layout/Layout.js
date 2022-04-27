f93ec68e30f2506ed12ec8e5840f1c6a2a3238bd
Ram Baghdadi <ram_baghdadi@hotmail.com>
2022-04-27 21:25:04 +0100

    attempt 2 page transition

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -5,29 +5,33 @@ export default function Layout({ children }) {
 	const router = useRouter()
 
 	return (
-		<LazyMotion features={domAnimation}>
-			<AnimatePresence initial={false} exitBeforeEnter={false}>
-				<m.div
-					key={router.route.concat(slideLeft.name)}
-					initial="initial"
-					animate="animate"
-					exit="exit"
-					variants={slideLeft.variants}
-					transition={{ duration: 0.8 }}
-					style={{
-						position: "absolute",
-						top: 0,
-						left: 0,
-						right: 0,
-						bottom: 0,
-						minWidth: "100%",
-						minHeight: "100%",
-					}}
-				>
-					{children}
-				</m.div>
-			</AnimatePresence>
-		</LazyMotion>
+		<div style={{ height: "100vh", width: "100vw" }}>
+			<div style={{ position: "absolute", height: "100%", width: "100vw" }}>
+				<LazyMotion features={domAnimation}>
+					<AnimatePresence initial={false} exitBeforeEnter={false}>
+						<m.div
+							key={router.route.concat(slideLeft.name)}
+							initial="initial"
+							animate="animate"
+							exit="exit"
+							variants={slideLeft.variants}
+							transition={{ duration: 0.8 }}
+							style={{
+								position: "absolute",
+								top: 0,
+								left: 0,
+								right: 0,
+								bottom: 0,
+								minWidth: "100%",
+								minHeight: "100%",
+							}}
+						>
+							{children}
+						</m.div>
+					</AnimatePresence>
+				</LazyMotion>
+			</div>
+		</div>
 	)
 }
 
