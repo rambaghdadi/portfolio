dce7a00fb09b29d705758585b40c0aec944722f5
Ram Baghdadi <ram_baghdadi@hotmail.com>
2022-04-28 20:01:37 +0100

    added site examples for portfolio

---
+++ components/PortfolioComponents/SiteExample/SiteExample.js
@@ -0,0 +1,29 @@
+import classes from "./SiteExample.module.css"
+
+export default function SiteExample(props) {
+	return (
+		<section className={classes.section}>
+			<div className={classes.container}>
+				<div className={classes.text}>
+					<h1>{props.title}</h1>
+					<h2>{props.subTitle}</h2>
+					<p>{props.main}</p>
+					<p className={classes.link}>
+						Visit:
+						<a target="_blank" href={props.link}>
+							{props.linkName}
+						</a>
+					</p>
+				</div>
+				<div>
+					<img
+						src={`/images/sites/${props.src}`}
+						alt="website"
+						width="300px"
+						height={"100%"}
+					/>
+				</div>
+			</div>
+		</section>
+	)
+}
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,11 +1,36 @@
 import Footer from "../components/General/Footer/Footer"
+import Introduction from "../components/General/Introduction/Introduction"
 import MainHero from "../components/General/MainHero/MainHero"
+import SiteExample from "../components/PortfolioComponents/SiteExample/SiteExample"
+import examples from "../lib/PortfolioExamples"
 
 export default function Portfolio(props) {
 	return (
 		<>
 			<div className="portfolio-page">
 				<MainHero text={"Work."} chubbs={"2"} />
+				<Introduction
+					title={"Projects"}
+					main={
+						"Below you will find a list of projects I worked on since I started learning web development."
+					}
+					text={
+						"Each list item will outline the technologies used to build it. Feel free to click-through to the actual websites for a more hands on experience."
+					}
+				/>
+				{examples.map((example) => {
+					return (
+						<SiteExample
+							key={example.title}
+							title={example.title}
+							subTitle={example.subTitle}
+							main={example.main}
+							link={example.link}
+							linkName={example.linkName}
+							src={example.src}
+						/>
+					)
+				})}
 				<Footer />
 			</div>
 		</>
