aebc31a198a170b394ac5354b5a58a6d8fa19513
Ram Baghdadi <ram_baghdadi@hotmail.com>
2023-08-13 12:23:27 +0100

    updated project list on mobile css

--- pages/index.js
+++ pages/index.js
@@ -5,24 +5,24 @@ import RecentWork from "../components/HomeComponents/RecentWork/RecentWork"
 import ChubbsCoffee from "../components/HomeComponents/ChubbsCoffee"
 
 export default function Home() {
-	return (
-		<>
-			<div className="homePage">
-				<MainHero text={"Who."}>
-					<ChubbsCoffee />
-				</MainHero>
-				<Introduction
-					title={"Welcome"}
-					main={
-						"My name is Ram, and I am a self-taught front-end web developer."
-					}
-					text={
-						"I created this website to house the different projects I  created since I started programming in late 2021. Please feel free to browse, and reach out with any work opportunities."
-					}
-				/>
-				<RecentWork />
-				<Footer />
-			</div>
-		</>
-	)
+  return (
+    <>
+      <div className="homePage">
+        <MainHero text={"Who."}>
+          <ChubbsCoffee />
+        </MainHero>
+        <Introduction
+          title={"Welcome"}
+          main={
+            "My name is Ram, and I am a self-taught front-end web developer."
+          }
+          text={
+            "I created this website to house the different projects I created since I started programming in late 2021. Please feel free to browse, and reach out with any work opportunities."
+          }
+        />
+        <RecentWork />
+        <Footer />
+      </div>
+    </>
+  )
 }
