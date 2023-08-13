cbfc831c613fbf512a45258e49e379d7430a6881
Ram Baghdadi <ram_baghdadi@hotmail.com>
2023-08-13 02:48:33 +0100

    added react component library and updated other descriptions

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import { AnimatePresence } from "framer-motion"
+import {AnimatePresence} from "framer-motion"
 import Head from "next/head"
 import Script from "next/script"
-import { useEffect, useState } from "react"
+import {useEffect, useState} from "react"
 import ContactMeModal from "../components/General/ContactMe/ContactMeModal"
 import Navbar from "../components/General/Navbar/Navbar"
 import ScrollUp from "../components/General/ScrollUp/ScrollUp"
@@ -10,78 +10,78 @@ import "../styles/globals.css"
 
 //TODO add recent projects
 
-function MyApp({ Component, pageProps }) {
-	const [openModal, setOpenModal] = useState(false)
-	const [width, setWidth] = useState(0) // default width, detect on server.
+function MyApp({Component, pageProps}) {
+  const [openModal, setOpenModal] = useState(false)
+  const [width, setWidth] = useState(0) // default width, detect on server.
 
-	const handleResize = () => setWidth(window.innerWidth)
+  const handleResize = () => setWidth(window.innerWidth)
 
-	useEffect(() => {
-		setWidth(window.innerWidth)
-		window.addEventListener("resize", handleResize)
-		return () => window.removeEventListener("resize", handleResize)
-	}, [handleResize])
+  useEffect(() => {
+    setWidth(window.innerWidth)
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [handleResize])
 
-	return (
-		<>
-			<Head>
-				<meta
-					name="viewport"
-					content="width=device-width"
-					initial-scale="1.0"
-				/>
-				<meta name="description" content="Ram's Portfolio" />
-				<meta
-					name="keywords"
-					content="Ram, Portfolio, Junior, Frontend, Web, Development"
-				/>
-				<link rel="icon" href="/images/Ram-logos_transparent.png" />
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width"
+          initial-scale="1.0"
+        />
+        <meta name="description" content="Ram's Portfolio" />
+        <meta
+          name="keywords"
+          content="Ram, Portfolio, Junior, Frontend, Web, Development"
+        />
+        <link rel="icon" href="/images/Ram-logos_transparent.png" />
 
-				<title>Ram - Portfolio</title>
-			</Head>
-			<Navbar
-				onClick={() => {
-					setOpenModal(true)
-				}}
-			/>
-			<AnimatePresence exitBeforeEnter={true}>
-				{openModal && (
-					<ContactMeModal
-						exit={() => {
-							setOpenModal(false)
-						}}
-					/>
-				)}
-			</AnimatePresence>
-			<ScrollUp />
+        <title>Ram - Portfolio</title>
+      </Head>
+      <Navbar
+        onClick={() => {
+          setOpenModal(true)
+        }}
+      />
+      <AnimatePresence mode="sync">
+        {openModal && (
+          <ContactMeModal
+            exit={() => {
+              setOpenModal(false)
+            }}
+          />
+        )}
+      </AnimatePresence>
+      <ScrollUp />
 
-			{width > 600 ? (
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			) : (
-				<Component {...pageProps} />
-			)}
-			<Script
-				id="g-analytics-script-1"
-				strategy="afterInteractive"
-				async
-				src="https://www.googletagmanager.com/gtag/js?id=G-QC967G8X09"
-			/>
-			<Script
-				id="g-analytics-script-2"
-				strategy="afterInteractive"
-				dangerouslySetInnerHTML={{
-					__html: `
+      {width > 600 ? (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      ) : (
+        <Component {...pageProps} />
+      )}
+      <Script
+        id="g-analytics-script-1"
+        strategy="afterInteractive"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-QC967G8X09"
+      />
+      <Script
+        id="g-analytics-script-2"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
 						window.dataLayer = window.dataLayer || [];
 						function gtag(){dataLayer.push(arguments);}
 						gtag('js', new Date());
 					  
 						gtag('config', 'G-QC967G8X09');`,
-				}}
-			/>
-		</>
-	)
+        }}
+      />
+    </>
+  )
 }
 
 export default MyApp
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -5,37 +5,37 @@ import SiteExample from "../components/PortfolioComponents/SiteExample/SiteExamp
 import examples from "../lib/PortfolioExamples"
 import ChubbsConfetti from "../components/PortfolioComponents/ChubbsConfetti"
 
-export default function Portfolio(props) {
-	return (
-		<>
-			<div className="portfolio-page">
-				<MainHero text={"Work."}>{<ChubbsConfetti />}</MainHero>
-				<Introduction
-					title={"Projects"}
-					main={
-						"Below you will find a list of projects I created since I started web development."
-					}
-					text={
-						"Each list item will outline the technologies used to build it. Feel free to click-through to the actual websites for a more hands on experience."
-					}
-				/>
-				<div className="sites-section">
-					{examples.map((example) => {
-						return (
-							<SiteExample
-								key={example.title}
-								title={example.title}
-								subTitle={example.subTitle}
-								main={example.main}
-								link={example.link}
-								linkName={example.linkName}
-								src={example.src}
-							/>
-						)
-					})}
-				</div>
-				<Footer />
-			</div>
-		</>
-	)
+export default function Portfolio() {
+  return (
+    <>
+      <div className="portfolio-page">
+        <MainHero text={"Work."}>{<ChubbsConfetti />}</MainHero>
+        <Introduction
+          title={"Projects"}
+          main={
+            "Below you will find a list of projects I created since I started web development."
+          }
+          text={
+            "Each list item will outline the technologies used to build it. Feel free to click-through to the actual websites for a more hands on experience."
+          }
+        />
+        <div className="sites-section">
+          {examples.map((example) => {
+            return (
+              <SiteExample
+                key={example.title}
+                title={example.title}
+                subTitle={example.subTitle}
+                main={example.main}
+                link={example.link}
+                linkName={example.linkName}
+                src={example.src}
+              />
+            )
+          })}
+        </div>
+        <Footer />
+      </div>
+    </>
+  )
 }
